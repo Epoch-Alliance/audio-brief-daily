804ea61333ef56d4af638379b1728c674cf9ec93
KevinTss <kevin.tassi@gmail.com>
2025-05-26 22:12:23 +0200

    remove useless plugin to copy index file

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { componentTagger } from 'lovable-tagger';
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,21 +10,9 @@ export default defineConfig(({ mode }) => ({
     host: '::',
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-    {
-      name: 'copy-index-to-404',
-      closeBundle: () => {
-        const distDir = path.resolve(__dirname, 'dist');
-        const indexPath = path.join(distDir, 'index.html');
-        const notFoundPath = path.join(distDir, '404.html');
-        if (fs.existsSync(indexPath)) {
-          fs.copyFileSync(indexPath, notFoundPath);
-        }
-      },
-    },
-  ].filter(Boolean),
+  plugins: [react(), mode === 'development' && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
