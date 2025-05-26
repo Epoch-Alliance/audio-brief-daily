f40c9f16bc6b81edcb8bae7809f06b939d26bcf1
KevinTss <kevin.tassi@gmail.com>
2025-05-26 21:48:47 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: mode === 'production' ? './audio-brief-daily/' : '/',
-  base: mode === 'production' ? './' : '/',
+  base: mode === 'production' ? './audio-brief-daily/' : '/',
   server: {
     host: '::',
     port: 8080,
